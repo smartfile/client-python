055d19619c1b43126f3bd83b52298eda53836e01
Sean C. Farley <sean-dev@farley.org>
2012-11-01 21:22:43 -0400

    Client package for v2 of the SmartFile API
    Add 'api' which is the Python package that interfaces with v2 of the
    SmartFile API.  Also, add unit tests for the package.  The unit tests
    can be run as:  python -m unittest tests
    
    This commit only provides a subset of the SmartFile API:
    - SmartFileUser (create, list, update, delete)
    - SmartFilePath (upload, delete, list (alias to path_tree.list)
    - SmartFilePathOperApi (remove)
    - SmartFilePathTreeApi (list)
    
    Instantiating SmartFileAPI provides access to all of the above API
    segments.  For example, api = SmartFileAPI() gives:
    - SmartFileUser:  api.user
    - SmartFilePath:  api.path
    - SmartFilePathOperApi:  api.path_oper
    - SmartFilePathTreeApi:  api.path_tree

---
+++ api/__init__.py
@@ -0,0 +1,198 @@
+import requests
+import time
+
+from os.path import basename
+from os.path import dirname
+from urlparse import urljoin
+
+
+class BaseSmartFileAPI(object):
+    """ Base class for specific API endpoints (i.e., user, path). """
+    _baseurl = 'http://localhost:8000/api/2/'
+
+    def __init__(self, api_key, api_pass, session=None, **kwargs):
+        # Re-use existing session if provided.
+        if session is None:
+            self._session = requests.session(auth=(api_key, api_pass))
+        else:
+            self._session = session
+
+    def _gen_url(self, uri_args=(), baseurl=None):
+        """ Join segments onto URL to call API. """
+        if baseurl:
+            url = baseurl
+        else:
+            url = urljoin(self._baseurl, self._api_uri)
+
+        # Concatenate the additional path components.
+        for arg in uri_args:
+            if arg != '/':
+                sep = '' if url.endswith('/') else '/'
+                url = '{0}{1}{2}'.format(url, sep, arg)
+
+        return url
+
+    def _create(self, data=None, *args, **kwargs):
+        """ The C in CRUD. """
+        url = self._gen_url(args, baseurl=kwargs.pop('baseurl', None))
+        return self._session.post(url, data=data, **kwargs)
+
+    def _read(self, *args, **kwargs):
+        """ The R in CRUD. """
+        url = self._gen_url(args, baseurl=kwargs.pop('baseurl', None))
+        return self._session.get(url, **kwargs)
+
+    def _update(self, data=None, *args, **kwargs):
+        """ The U in CRUD. """
+        url = self._gen_url(args, baseurl=kwargs.pop('baseurl', None))
+        if data is not None:
+            kwargs['data'] = data
+        return self._session.post(url, **kwargs)
+
+    def _delete(self, *args, **kwargs):
+        """ The D in CRUD. """
+        url = self._gen_url(args, baseurl=kwargs.pop('baseurl', None))
+        return self._session.delete(url, **kwargs)
+
+
+class SmartFileUserAPI(BaseSmartFileAPI):
+    """ User API. """
+    _api_uri = 'user/'
+
+    @property
+    def create(self):
+        return self._create
+
+    @property
+    def list(self):
+        return self._read
+
+    @property
+    def update(self):
+        return self._update
+
+    @property
+    def delete(self):
+        return self._delete
+
+
+class SmartFilePathOperAPI(BaseSmartFileAPI):
+    """ Path Oper API. """
+    _api_uri = 'path/oper/'
+
+    def remove(self, path):
+        """ Create task to remove file system object(s). """
+        return super(SmartFilePathOperAPI, self)._create(
+            {'path': path}, 'remove/')
+
+    def poll(self, url, checks=5, check_timeout=2):
+        """
+        Poll a URL until a non-200 response or the result of operation is
+        SUCCESS.  Check a few times with a sleep between each check.
+        """
+        while checks > 0:
+            response = self._session.get(url)
+            if (response.status_code != 200 or
+                response.json['result']['status'] == 'SUCCESS'):
+                break
+            checks -= 1
+            time.sleep(check_timeout)
+
+        return response
+
+
+class SmartFilePathTreeAPI(BaseSmartFileAPI):
+    """ Path Tree API. """
+    _api_uri = 'path/tree/'
+
+    def list(self, path='/', children=False, *args, **kwargs):
+        if children:
+            kwargs['params'] = {'children': True}
+        return super(SmartFilePathTreeAPI, self)._read(path, *args, **kwargs)
+
+
+class SmartFilePathAPI(BaseSmartFileAPI):
+    """ Path API. """
+    _api_uri = 'path/'
+    _api_uri_ext = 'data/'
+
+    def __init__(self, *args, **kwargs):
+        super(SmartFilePathAPI, self).__init__(*args, **kwargs)
+        kwargs['session'] = self._session
+        self._path_oper_api = SmartFilePathOperAPI(*args, **kwargs)
+        self._path_tree_api = SmartFilePathTreeAPI(*args, **kwargs)
+
+    def _get_file_data(self, id, data=False):
+        """ Get data of file using ID of file. """
+        args = (id, self._api_uri_ext) if data else (id, )
+        return super(SmartFilePathAPI, self)._read(*args)
+
+    @property
+    def list(self):
+        """ Shortcut to Path Tree API list(). """
+        return self._path_tree_api.list
+
+    def remove(self, path):
+        """ Remove the file and poll awhile for it to finish. """
+        response = self._path_oper_api.remove(path)
+        if response.status_code == 200:
+            response = self._path_oper_api.poll(response.json['url'])
+        return response
+
+    def download(self, dst, src):
+        # Get file ID and download file in chunks.
+        tree = self.list(src)
+        response = self._get_file_data(tree.json['id'], True)
+        if response.status_code == 200:
+            with open(dst, 'wb') as dst_file:
+                for chunk in response.iter_content(16 * 1024):
+                    dst_file.write(chunk)
+
+        return response
+
+    def upload(self, dst, src):
+        # Get directory ID.
+        dst_dir = dirname(dst)
+        tree = self._path_tree_api.list(dst_dir)
+
+        # Upload file.
+        files = {'file': (basename(dst), open(src, 'rb'))}
+        return super(SmartFilePathAPI, self)._create(
+            None, self._api_uri_ext, baseurl=tree.json['url'], files=files)
+
+
+class SmartFileAPI(object):
+    """
+    This class provides a single interface to the various segments of the
+    SmartFile API.
+    """
+    _session = None
+
+    def __init__(self, api_key, api_pass):
+        self._api_key = api_key
+        self._api_pass = api_pass
+
+    def _get_api(self, attr, cls):
+        api = getattr(self, attr, None)
+        if api is None:
+            api = cls(self._api_key, self._api_pass, session=self._session)
+            setattr(self, attr, api)
+            if self._session is None:
+                self._session = getattr(self, attr)._session
+        return api
+
+    @property
+    def path(self):
+        return self._get_api('_api_path_obj', SmartFilePathAPI)
+
+    @property
+    def path_oper(self):
+        return self._get_api('_api_path_oper_obj', SmartFilePathOperAPI)
+
+    @property
+    def path_tree(self):
+        return self._get_api('_api_path_tree_obj', SmartFilePathTreeAPI)
+
+    @property
+    def user(self):
+        return self._get_api('_api_user_obj', SmartFileUserAPI)
---
+++ tests/__init__.py
@@ -0,0 +1,135 @@
+import filecmp
+import os
+import unittest
+
+from api import SmartFileAPI
+
+
+# NOTE:  replace API key and password with actual values.
+API_KEY = 'tqpPpZqotBMojYX'
+API_PASS = 'JC5c3bKwhNkBAAk'
+
+
+class BaseAPITestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.client = SmartFileAPI(API_KEY, API_PASS)
+
+
+class UserTestCase(BaseAPITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(UserTestCase, cls).setUpClass()
+        cls._test_user = {
+            'name': 'Test User',
+            'username': 'testuser',
+            'password': 'testpass',
+            'email': 'testuser@example.com'
+        }
+        cls._test_user2 = {
+            'name': 'Test User2',
+            'username': 'testuser2',
+            'password': 'testpass2',
+            'email': 'testuser2@example.com'
+        }
+        cls.client.user.delete(cls._test_user['username'])
+        cls.client.user.delete(cls._test_user2['username'])
+
+    def test_create_user(self):
+        response = self.client.user.create(self._test_user2)
+        self.assertEqual(response.status_code, 201)
+        self.client.user.delete(self._test_user2['username'])
+
+    def test_list_users(self):
+        self.client.user.create(self._test_user)
+        response = self.client.user.list()
+        self.assertEqual(response.status_code, 200)
+        self.client.user.delete(self._test_user['username'])
+
+    def test_list_user(self):
+        self.client.user.create(self._test_user)
+        response = self.client.user.list(self._test_user['username'])
+        self.assertEqual(response.status_code, 200)
+        self.client.user.delete(self._test_user['username'])
+
+    def test_delete_user(self):
+        self.client.user.create(self._test_user)
+        response = self.client.user.delete(self._test_user['username'])
+        self.assertEqual(response.status_code, 204)
+
+
+class BasePathTestCase(BaseAPITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(BasePathTestCase, cls).setUpClass()
+        cls.local_file = '/etc/motd'
+        cls.remote_file = '/motd'
+        cls.downloaded_file = 'motd.down'
+        cls.client.path.remove(cls.remote_file)
+
+
+class PathTestCase(BasePathTestCase):
+    def test_file_upload(self):
+        response = self.client.path.upload(self.remote_file, self.local_file)
+        self.assertEqual(response.status_code, 200)
+        self.client.path.remove(self.remote_file)
+
+    def test_file_download(self):
+        self.client.path.upload(self.remote_file, self.local_file)
+        response = self.client.path.download(self.downloaded_file,
+                                             self.remote_file)
+        cmp_result = filecmp.cmp(self.local_file, self.downloaded_file)
+        try:
+            os.unlink(self.downloaded_file)
+        except:
+            pass
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(cmp_result)
+        self.client.path.remove(self.remote_file)
+
+    def test_file_remove(self):
+        """ Delete file using shortcut within Path API. """
+        self.client.path.upload(self.remote_file, self.local_file)
+        response = self.client.path.remove(self.remote_file)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['result']['status'], 'SUCCESS')
+
+
+class PathOperTestCase(BasePathTestCase):
+    def test_file_remove(self):
+        # Create task to delete file.
+        self.client.path.upload(self.remote_file, self.local_file)
+        response = self.client.path_oper.remove(self.remote_file)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the task completed.
+        response = self.client.path_oper.poll(response.json['url'])
+        status = response.json['result']['status']
+        self.assertEqual(status, 'SUCCESS')
+
+
+class PathTreeTestCase(BasePathTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(PathTreeTestCase, cls).setUpClass()
+        cls.local_file = '/etc/motd'
+        cls.remote_file = '/motd'
+        cls.downloaded_file = 'motd.down'
+        cls.client.path.remove(cls.remote_file)
+
+    def test_file_list(self):
+        self.client.path.upload(self.remote_file, self.local_file)
+        response = self.client.path_tree.list(self.remote_file)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['path'], self.remote_file)
+
+    def test_directory_list(self):
+        response = self.client.path_tree.list('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['path'], '/')
+
+    def test_directory_and_children_list(self):
+        response = self.client.path_tree.list('/', children=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('children', response.json)
+        self.assertGreater(len(response.json['children']), 0)
