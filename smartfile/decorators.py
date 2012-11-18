a5233bfe8495bb03a8656f0b78f9ca7f838f34b4
Sean C. Farley <sean-dev@farley.org>
2012-11-18 16:22:20 -0500

    Limit to time to receive non-throttled response
    Add a limit to how long the client will continue to attempt a
    non-throttled response from the server.  This prevents a request from
    never returning or at least not returning in a timely manner assuming
    the server continues to throttle the client.
    
    This could happen if either:
    - The server get stuck on return 503's to the client.
    - There are simultaneous requests to the server using the API key and
      password of the client that cause this particular request to keep
      getting throttled.

--- smartfile/decorators.py
+++ smartfile/decorators.py
@@ -9,13 +9,16 @@ from smartfile.exceptions import SmartFileConnException
 from smartfile.exceptions import SmartFileResponseException
 
 
-def throttle_wait(func=None, throttle=True):
+def throttle_wait(func=None, throttle=True, max_wait=120):
     """
     Make a request but try again if the client has hit the throttle.  This can
     made to just make the request by passing False for throttle.  The time to
     wait to make another attempt is taken from the time passed back by the
     server within the 'x-throttle' header.  This call will not return until it
     receives a non-throttled response.
+
+    The max_wait argument is the total time since the first request was made
+    that the client will make attempts to receive a non-throttled response.
 
     Usage:
         @throttle_wait
@@ -26,12 +29,16 @@ def throttle_wait(func=None, throttle=True):
             @wraps(func)
             def _throttle_wait(*args, **kwargs):
                 _throttle = throttle
-                while True:
+                start = time.time()
+                end = 0
+                while end - start < max_wait:
                     response = func(*args, **kwargs)
+
                     if _throttle and response.status_code == 503:
                         wait = re.search('next=([^ ]+) sec',
                                          response.headers['x-throttle'])
                         time.sleep(float(wait.group(1)))
+                        end = time.time()
                     else:
                         break
                 return response
